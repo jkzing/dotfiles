6e84d6ae162690165e58cdfa6195fa8f88d54345
JK <jingkai.zhao@foxmail.com>
2022-07-11 17:38:45 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,6 +10,9 @@ ln -s -f ~/.tmux/.tmux.conf ~/.tmux.conf
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
+# pnpm https://pnpm.io/installation
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+
 # copy dotfiles
 cp -r .config ~/.config
 cp .zshrc ~/.zshrc
